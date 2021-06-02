e6fa0a95d5cb15e01daa09c4e3d4fffb19409189
tgabor7 <tgabor7@gmail.com>
2021-06-02 18:50:07 +0200

    ready for deploy

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res)=>{
+	res.send('Hi')
+})
+
+app.listen(3000, ()=>{
+	console.log('App listenning on port 3000')
+})
